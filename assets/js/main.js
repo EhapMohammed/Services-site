a82268b5f4272e5c493f1472e571d88291e6a0d2
unknown <ehapmohammad97@gmail.com>
2023-08-12 20:32:21 +0300

    add services page for admin english version only

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,12 @@ calendarSettingsToggler.addEventListener('click', function() {
     calendarSettingsExtra.style.display = "none";
   }
 });
+
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
